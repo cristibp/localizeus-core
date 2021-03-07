b3674e3fc3860f63b66108ae233945a421ccd2e3
Alexandra Binca <alexandra.binca@gmail.com>
2021-03-07 22:03:23 +0200

    mysql updated

--- src/main/docker/init.sql
+++ src/main/docker/init.sql
@@ -1,3 +1,8 @@
+FLUSH PRIVILEGES;
+drop user if exists 'localizeus'@'%';
+
+create user 'localizeus'@'%' IDENTIFIED BY 'localizeus' ;
+GRANT ALL PRIVILEGES ON * . * TO 'localizeus'@'%';
+FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS localizeus;
 use localizeus;
-GRANT ALL PRIVILEGES ON *.* TO 'localizeus'@'%';
